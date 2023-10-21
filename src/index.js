9b23c24e6768ff834f6ebd6e85df2fc8907315f6
Eren Gazioglu <pompyproduction@gmail.com>
2023-10-22 01:24:09 +0200

    Create webpack config file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    // config goes here
+}
+const firebaseApp = initializeApp(firebaseConfig);
+
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, "src/index.js")
+    },
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+        assetModuleFilename: "[name][ext]"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.(jpe?g|png|svg|gif)$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            // title: "Webpack App",
+            filename: "index.html",
+            template: path.resolve(__dirname, "src/template.html")
+        })
+    ],
+    devtool: "source-map",
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        historyApiFallback: true,
+        open: true,
+        hot: true
+    },
+    // optimization: {
+    //     runtimeChunk: 'single',
+    // },
+  };
